3ca33a5881457ea457b2190b4af93ebe2e7c06e2
Indresh-Prakash <indresh.prakash@freshworks.com>
2022-06-06 15:33:07 +0530

    Remove unwanted code

--- lib/deadlock_retry.rb
+++ lib/deadlock_retry.rb
@@ -10,8 +10,6 @@ module DeadlockRetry
     end
   end
 
-  mattr_accessor :innodb_status_cmd
-
   module ClassMethods
     DEADLOCK_ERROR_MESSAGES = [
       "Deadlock found when trying to get lock",
@@ -25,8 +23,6 @@ module DeadlockRetry
     def transaction_with_deadlock_handling(*objects, &block)
       retry_count = 0
 
-      #check_innodb_status_available
-
       begin
         transaction_without_deadlock_handling(*objects, &block)
       rescue ActiveRecord::StatementInvalid => error
@@ -35,7 +31,6 @@ module DeadlockRetry
           raise if retry_count >= MAXIMUM_RETRIES_ON_DEADLOCK
           retry_count += 1
           logger.info "Deadlock detected on retry #{retry_count}, restarting transaction"
-          #log_innodb_status if DeadlockRetry.innodb_status_cmd
           exponential_pause(retry_count)
           retry
         else
@@ -60,48 +55,8 @@ module DeadlockRetry
       connection.open_transactions != 0
     end
 
-    def show_innodb_status
-       self.connection.select_value(DeadlockRetry.innodb_status_cmd)
-    end
-
     # Should we try to log innodb status -- if we don't have permission to,
     # we actually break in-flight transactions, silently (!)
-    def check_innodb_status_available
-      return unless DeadlockRetry.innodb_status_cmd == nil
-
-      if self.connection.adapter_name == "Mysql2"
-        begin
-          mysql_version = self.connection.select_rows('show variables like \'version\'')[0][1]
-          cmd = if mysql_version < '5.5'
-            'show innodb status'
-          else
-            'show engine innodb status'
-          end
-          self.connection.select_value(cmd)
-          DeadlockRetry.innodb_status_cmd = cmd
-        rescue
-          logger.info "Cannot log innodb status: #{$!.message}"
-          DeadlockRetry.innodb_status_cmd = false
-        end
-      else
-        DeadlockRetry.innodb_status_cmd = false
-      end
-    end
-
-    def log_innodb_status
-      # show innodb status is the only way to get visiblity into why
-      # the transaction deadlocked.  log it.
-      lines = show_innodb_status
-      logger.error "INNODB Status follows:"
-      NewRelic::Agent.notice_error(ActiveRecord::StatementInvalid,{:custom_params => {:error => "Mysql deadlock",
-                                                                                      :innodb_status => lines }})
-      lines.each_line do |line|
-        logger.error line
-      end
-    rescue => e
-      # Access denied, ignore
-      logger.info "Cannot log innodb status: #{e.message}"
-    end
 
   end
 end
