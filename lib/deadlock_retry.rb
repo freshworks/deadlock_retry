40fd3e51aa34ef302bac6503a6fd270810b6d8a7
Murugaperumal Ramasamy <murugaperumal.ramasamy@freshworks.com>
2025-09-26 22:46:26 +0530

    Use exception message

--- lib/deadlock_retry.rb
+++ lib/deadlock_retry.rb
@@ -2,28 +2,37 @@ require 'active_support/core_ext/module/attribute_accessors'
 
 module DeadlockRetry
   MAXIMUM_RETRIES_ON_DEADLOCK = 3
+  DEADLOCK_ERROR_MESSAGES = [
+    "Deadlock found when trying to get lock",
+    "Lock wait timeout exceeded",
+    "deadlock detected"
+  ]
 
   def transaction(requires_new: nil, isolation: nil, joinable: true, &block)
     retry_count = 0
 
     begin
       super(requires_new: requires_new, isolation: isolation, joinable: joinable, &block)
-    rescue ActiveRecord::LockWaitTimeout, ActiveRecord::Deadlocked => e
+    rescue ActiveRecord::StatementInvalid => error
       if in_nested_transaction?
-        logger.info { "Deadlock detected in a nested transaction, not retrying." }
+        ::Rails.logger.info "Deadlock detected in a nested transaction, not retrying."
         raise
       end
 
-      if retry_count >= MAXIMUM_RETRIES_ON_DEADLOCK
-        logger.info { "Deadlock detected and maximum retries exceeded (maximum: #{MAXIMUM_RETRIES_ON_DEADLOCK}), not retrying." }
+      if DEADLOCK_ERROR_MESSAGES.any? { |msg| error.message =~ /#{Regexp.escape(msg)}/ }
+        if retry_count >= MAXIMUM_RETRIES_ON_DEADLOCK
+          ::Rails.logger.info "Deadlock detected and maximum retries exceeded (maximum: #{MAXIMUM_RETRIES_ON_DEADLOCK}), not retrying."
+          raise
+        end
+
+        retry_count += 1
+        pause_seconds = exponential_pause_seconds(retry_count)
+        ::Rails.logger.info "Deadlock detected on retry #{retry_count}, retrying transaction in #{pause_seconds} seconds."
+        sleep_pause(pause_seconds)
+        retry
+      else
         raise
       end
-
-      retry_count += 1
-      pause_seconds = exponential_pause_seconds(retry_count)
-      logger.info { "Deadlock detected on retry #{retry_count}, retrying transaction in #{pause_seconds} seconds." }
-      sleep_pause(pause_seconds)
-      retry
     end
   end
 
