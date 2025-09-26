f488ade56db8c1c34a0446a41bb3680c0ec2ddb6
Murugaperumal Ramasamy <murugaperumal.ramasamy@freshworks.com>
2025-09-26 15:58:49 +0530

    Update to Rails 6

--- lib/deadlock_retry.rb
+++ lib/deadlock_retry.rb
@@ -1,65 +1,51 @@
 require 'active_support/core_ext/module/attribute_accessors'
 
 module DeadlockRetry
-  def self.included(base)
-    base.extend(ClassMethods)
-    base.class_eval do
-      class << self
-        alias_method :transaction_without_deadlock_handling, :transaction
-        alias_method :transaction, :transaction_with_deadlock_handling
+  MAXIMUM_RETRIES_ON_DEADLOCK = 3
+
+  def transaction(requires_new: nil, isolation: nil, joinable: true, &block)
+    retry_count = 0
+
+    begin
+      super(requires_new: requires_new, isolation: isolation, joinable: joinable, &block)
+    rescue ActiveRecord::LockWaitTimeout, ActiveRecord::Deadlocked => e
+      if in_nested_transaction?
+        logger.info { "Deadlock detected in a nested transaction, not retrying. [#{e.class}]" }
+        raise
       end
+
+      if retry_count >= MAXIMUM_RETRIES_ON_DEADLOCK
+        logger.info { "Deadlock detected and maximum retries exceeded (maximum: #{MAXIMUM_RETRIES_ON_DEADLOCK}), not retrying. [#{e.class}]" }
+        raise
+      end
+
+      retry_count += 1
+      pause_seconds = exponential_pause_seconds(retry_count)
+      logger.info { "Deadlock detected on retry #{retry_count}, retrying transaction in #{pause_seconds} seconds. [#{e.class}]" }
+      sleep_pause(pause_seconds)
+      retry
     end
   end
 
-  module ClassMethods
-    DEADLOCK_ERROR_MESSAGES = [
-      "Deadlock found when trying to get lock",
-      "Lock wait timeout exceeded",
-      "deadlock detected"
-    ]
+  private
 
-    MAXIMUM_RETRIES_ON_DEADLOCK = 3
+  WAIT_TIMES = [0, 1, 2, 4, 8, 16, 32]
 
-
-    def transaction_with_deadlock_handling(*objects, &block)
-      retry_count = 0
-
-      begin
-        transaction_without_deadlock_handling(*objects, &block)
-      rescue ActiveRecord::StatementInvalid => error
-        raise if in_nested_transaction?
-        if DEADLOCK_ERROR_MESSAGES.any? { |msg| error.message =~ /#{Regexp.escape(msg)}/ }
-          raise if retry_count >= MAXIMUM_RETRIES_ON_DEADLOCK
-          retry_count += 1
-          logger.info "Deadlock detected on retry #{retry_count}, restarting transaction"
-          exponential_pause(retry_count)
-          retry
-        else
-          raise
-        end
-      end
-    end
-
-    private
-
-    WAIT_TIMES = [0, 1, 2, 4, 8, 16, 32]
-
-    def exponential_pause(count)
-      sec = WAIT_TIMES[count-1] || 32
-      # sleep 0, 1, 2, 4, ... seconds up to the MAXIMUM_RETRIES.
-      # Cap the pause time at 32 seconds.
-      sleep(sec) if sec != 0
-    end
-
-    def in_nested_transaction?
-      # open_transactions was added in 2.2's connection pooling changes.
-      connection.open_transactions != 0
-    end
-
-    # Should we try to log innodb status -- if we don't have permission to,
-    # we actually break in-flight transactions, silently (!)
-
+  def exponential_pause_seconds(count)
+    # sleep 0, 1, 2, 4, ... seconds up to the MAXIMUM_RETRIES.
+    # Cap the pause time at 32 seconds.
+    WAIT_TIMES[count-1] || 32
   end
+
+  def sleep_pause(seconds)
+    sleep(seconds) if seconds != 0
+  end
+
+  def in_nested_transaction?
+    # open_transactions was added in 2.2's connection pooling changes.
+    connection.open_transactions != 0
+  end
+
 end
 
-ActiveRecord::Base.send(:include, DeadlockRetry) if defined?(ActiveRecord)
+ActiveRecord::Base.singleton_class.send(:prepend, DeadlockRetry) if defined?(ActiveRecord)
